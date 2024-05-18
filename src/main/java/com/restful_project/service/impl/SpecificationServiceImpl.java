896e876cb6a77f9eab6bc8626475ce563a8668ad
Nastya <vnedostupa@yandex.ru>
2024-05-18 15:48:10 +0300

    some changes

--- src/main/java/com/restful_project/service/impl/SpecificationServiceImpl.java
+++ src/main/java/com/restful_project/service/impl/SpecificationServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SpecificationServiceImpl implements SpecificationService {
